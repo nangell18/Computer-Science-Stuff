bbed04d7371f5f39044c38231948ed7e2abde9d6
nangell18 <65080006+nangell18@users.noreply.github.com>
2022-09-12 10:04:12 -0400

    finished lesson 6, almost done with lesson 7

---
+++ py4e/lesson_7/looping_through_strings.py
@@ -0,0 +1,11 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index,letter)
+    index = index + 1
+
+#less code that you write, the less of a chance for me to make a mistake
+fruit1 = 'banana'
+for letter in fruit1:
+    print(letter)
