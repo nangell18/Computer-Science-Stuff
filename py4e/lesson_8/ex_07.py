8ea56c726d1bad7ef0d7ac0f17bdbe6234e288bf
nangell18 <65080006+nangell18@users.noreply.github.com>
2022-09-23 09:05:23 -0400

    all from lesson 8 part 2

--- py4e/lesson_8/ex_07.py
+++ py4e/lesson_8/ex_07.py
@@ -1,3 +1,33 @@
 xfile = open(r'C:\Users\student\Documents\GitHub\Computer-Science-Stuff\py4e\lesson_8\file.txt')
-print(xfile)
+#print(xfile)
 #print(xfile.read())
+
+# just read the entire file
+for cheese in xfile:
+    print(cheese)
+
+#you need to declare a different file name everytime you want to do something different to that file
+xfile1 = open(r'C:\Users\student\Documents\GitHub\Computer-Science-Stuff\py4e\lesson_8\file.txt')
+count = 0
+for line in xfile1:
+    count = count +1
+print('line count!!:',count, '\n\n')
+
+
+xfile2 = open(r'C:\Users\student\Documents\GitHub\Computer-Science-Stuff\py4e\lesson_8\file.txt')
+for line in xfile2:
+    line = line.rstrip() #get rid of the whitespace below
+    if line.startswith('From:'):
+        print(line)
+
+
+xfile3 = open(r'C:\Users\student\Documents\GitHub\Computer-Science-Stuff\py4e\lesson_8\file.txt')
+for line in xfile3:
+    line = line.rstrip()
+    if not '@uct.ac.za' in line: #if the string is not in line, then skip it and then print that line at the end
+        continue
+    print(line)
+
+
+
+    # use quit() function to stop the entire program all together
